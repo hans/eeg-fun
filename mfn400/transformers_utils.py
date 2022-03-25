af13997928aa71d3722e9a4be0b2ac51562db30d
Jon Gauthier <jon@gauthiers.net>
2022-03-25 12:54:47 -0400

    refactor out transformers_utils.compute_surprisals

--- mfn400/transformers_utils.py
+++ mfn400/transformers_utils.py
@@ -5,8 +5,70 @@ Utilities for working with Huggingface transformers models.
 from typing import List, Tuple
 
 import numpy as np
+import pandas as pd
 import torch
 import transformers
+
+
+def compute_surprisals(sentences: List[List[str]], model="gpt2") -> pd.DataFrame:
+    """
+    Compute word-level surprisals and other positional information. Returns a
+    dataframe with columns:
+
+        global_text_word_idx: Index into flattened token list of fulltext
+        sent_idx:
+        text_word_idx: Index into sentence word idx
+        tok_idx: Model token idx
+        surprisal: log-e surprisal
+    """
+    hf_tokenizer = transformers.AutoTokenizer.from_pretrained(model)
+    hf_model = transformers.AutoModelForCausalLM.from_pretrained(
+        model, add_cross_attention=True, is_decoder=True)
+
+    batch_encoding, sentence_surprisals = \
+        get_predictive_outputs(hf_model, hf_tokenizer, sentences)
+
+    global_tok_cursor = 0
+
+    # Each element is of form (global_text_tok_idx, sent_idx, text_tok_idx, tok_idx, surprisal)
+    # `global_text_tok_idx` is an index into `tokens_flat`
+    # `sent_idx` is an index into `sentences`
+    # `text_tok_idx` is an index into `sentences[sent_idx]`
+    surp_mapping = []
+    for sent_idx, sentence_surprisals in enumerate(sentence_surprisals):
+        sent_tokens = torch.tensor(batch_encoding["input_ids"][sent_idx])
+
+        # get surprisals of expected words
+        surps_shifted = sentence_surprisals[:-1, :]
+        sent_tokens_shifted = sent_tokens[1:]
+        token_surps = surps_shifted[range(surps_shifted.shape[0]), sent_tokens_shifted]
+
+        word_ids = batch_encoding.word_ids(sent_idx)
+        for idx, surp in enumerate(token_surps):
+            # We are enumerating the shifted list. Get the original token
+            # index.
+            tok_id = idx + 1
+            
+            if word_ids[tok_id] is None:
+                continue
+            elif word_ids[tok_id] >= len(sentences[sent_idx]):
+                # Word ID is out-of-bounds. This shouldn't happen. But
+                # sometimes it does, because Huggingface tokenizer imputes a
+                # different notion of "word" than what is in the pre-tokenized
+                # input sentence. See comment in `get_predictive_outputs` for
+                # a fix, if you really care about sentence-final surprisals.
+                continue
+                
+            surp_mapping.append((global_tok_cursor + word_ids[tok_id],
+                                 sent_idx, word_ids[tok_id], tok_id, surp))
+
+        global_tok_cursor += len(sentences[sent_idx])
+
+    surp_mapping = pd.DataFrame(surp_mapping,
+                                columns=["global_text_word_idx", "sent_idx",
+                                         "text_word_idx", "tok_idx",
+                                         "surprisal"])
+    return surp_mapping
 
 
 def get_predictive_outputs(model: transformers.PreTrainedModel,
--- scripts/frank2015_stimuli.py
+++ scripts/frank2015_stimuli.py
@@ -29,68 +29,6 @@ def load_stimuli(path) -> List[List[str]]:
     return [list(sentence) for sentence in data["sentences"]]
 
 
-def compute_surprisals(sentences: List[List[str]], model="gpt2"):
-    """
-    Compute word-level surprisals and other positional information. Returns a
-    dataframe with columns:
-
-        global_text_word_idx: Index into flattened token list of fulltext
-        sent_idx:
-        text_word_idx: Index into sentence word idx
-        tok_idx: Model token idx
-        surprisal: log-e surprisal
-    """
-    hf_tokenizer = transformers.AutoTokenizer.from_pretrained(model)
-    hf_model = transformers.AutoModelForCausalLM.from_pretrained(
-        model, add_cross_attention=True, is_decoder=True)
-
-    batch_encoding, sentence_surprisals = \
-        transformers_utils.get_predictive_outputs(hf_model, hf_tokenizer,
-                                                  tqdm(sentences))
-
-    global_tok_cursor = 0
-
-    # Each element is of form (global_text_tok_idx, sent_idx, text_tok_idx, tok_idx, surprisal)
-    # `global_text_tok_idx` is an index into `tokens_flat`
-    # `sent_idx` is an index into `sentences`
-    # `text_tok_idx` is an index into `sentences[sent_idx]`
-    surp_mapping = []
-    for sent_idx, sentence_surprisals in enumerate(sentence_surprisals):
-        sent_tokens = torch.tensor(batch_encoding["input_ids"][sent_idx])
-
-        # get surprisals of expected words
-        surps_shifted = sentence_surprisals[:-1, :]
-        sent_tokens_shifted = sent_tokens[1:]
-        token_surps = surps_shifted[range(surps_shifted.shape[0]), sent_tokens_shifted]
-
-        word_ids = batch_encoding.word_ids(sent_idx)
-        for idx, surp in enumerate(token_surps):
-            # We are enumerating the shifted list. Get the original token
-            # index.
-            tok_id = idx + 1
-            
-            if word_ids[tok_id] is None:
-                continue
-            elif word_ids[tok_id] >= len(sentences[sent_idx]):
-                # Word ID is out-of-bounds. This shouldn't happen. But
-                # sometimes it does, because Huggingface tokenizer imputes a
-                # different notion of "word" than what is in the pre-tokenized
-                # input sentence. See comment in `get_predictive_outputs` for
-                # a fix, if you really care about sentence-final surprisals.
-                continue
-                
-            surp_mapping.append((global_tok_cursor + word_ids[tok_id],
-                                 sent_idx, word_ids[tok_id], tok_id, surp))
-
-        global_tok_cursor += len(sentences[sent_idx])
-
-    surp_mapping = pd.DataFrame(surp_mapping,
-                                columns=["global_text_word_idx", "sent_idx",
-                                         "text_word_idx", "tok_idx",
-                                         "surprisal"])
-    return surp_mapping
-
-
 def add_control_predictors(stim_df, sentences):
     stim_df["word_len"] = stim_df.word.str.len()
 
@@ -123,7 +61,7 @@ def main(args):
     if args.surprisals_path:
         surp_mapping = pd.read_csv(args.surprisals_path)
     else:
-        surp_mapping = compute_surprisals(sentences, model=args.model)
+        surp_mapping = transformers_utils.compute_surprisals(sentences, model=args.model)
 
     # Merge stim df with surprisals.
     stim_df = pd.merge(stim_df.reset_index(),
